420e3245218f641c5ee32331b123a3a3cf4c7c09
Riya <riyahal.dev@gmail.com>
2025-09-21 17:44:51 +0530

    Update app.py

--- python_backend/app.py
+++ python_backend/app.py
@@ -31,4 +31,6 @@ def simplify():
     return jsonify({'simplified_document': simplified_doc})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
